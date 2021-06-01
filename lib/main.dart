7ebccaa2c5db47fbf7df076ce2de353c8ac993d6
danacealfonso <danace_bboy@gmail.com>
2021-06-01 15:18:34 +0800

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(KwartrackerApp());
+}
+
+class KwartrackerApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Kwartracker App',
+      home: MyHomePage(title: 'Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF03BED6),
+      appBar: AppBar(
+        title: Row(children: [
+          Text(
+            "Hello",
+          ),
+          Text(
+            "Samantha",
+          ),
+        ]),
+        centerTitle: true,
+        actions: [
+          Container(child: ConstrainedBox(
+              constraints: BoxConstraints.expand(),
+              child: TextButton(
+                  onPressed: null,
+                  child: Image.asset('images/users/profile_pic.png')
+              )
+          )
+          )
+        ],
+        elevation: 0,
+        backgroundColor: Color(0xFF03BED6),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
